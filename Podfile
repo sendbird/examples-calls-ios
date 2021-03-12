55a4314f253115707eff7fef7bf83324e86f5e38
Minhyuk Kim <kimminhyuk1004@gmail.com>
2021-03-12 20:05:16 +0900

    Initialize project

--- Podfile
+++ Podfile
@@ -3,6 +3,7 @@ platform :ios, '9.0'
 workspace 'SendBirdCallsExample'
 project 'ScreenCaptureExample/ScreenCaptureExample.xcodeproj'
 project 'ScreenRecordExample/ScreenRecordExample.xcodeproj'
+project 'ScreenShareExample/ScreenShareExample.xcodeproj'
 
 target 'ScreenCaptureExample' do
     project 'ScreenCaptureExample/ScreenCaptureExample.xcodeproj'
@@ -11,5 +12,10 @@ end
 
 target 'ScreenRecordExample' do
     project 'ScreenRecordExample/ScreenRecordExample.xcodeproj'
+    pod 'SendBirdCalls'
+end
+
+target 'ScreenShareExample' do
+    project 'ScreenShareExample/ScreenShareExample.xcodeproj'
     pod 'SendBirdCalls'
 end
---
+++ ScreenShareExample/ScreenShareExample/AppDelegate.swift
@@ -0,0 +1,54 @@
+//
+//  AppDelegate.swift
+//  ScreenShareExample
+//
+//  Created by Minhyuk Kim on 2021/03/12.
+//
+
+import UIKit
+import SendBirdCalls
+import Commons
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let appId: String = <#Application ID From Sendbird Dashboard#>
+        SendBirdCall.configure(appId: appId)
+        SendBirdCall.addDelegate(self, identifier: "AppDelegate")
+        
+        return true
+    }
+}
+
+// Process incoming call
+extension AppDelegate: SendBirdCallDelegate {
+    func didStartRinging(_ call: DirectCall) {
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+        guard let callerId = call.caller?.userId,
+              let viewController = storyboard.instantiateViewController(withIdentifier: "CallingViewController") as? CallingViewController else { return }
+        
+        let alertController = UIAlertController(title: "Incoming Call",
+                                           message: "Incoming \(call.isVideoCall ? "Video" : "Audio") Call from \(callerId)",
+                                           preferredStyle: .alert)
+        
+        let acceptAction = UIAlertAction(title: "Accept", style: .default, handler: { (_) in
+            call.accept(with: AcceptParams(callOptions: CallOptions(isAudioEnabled: true)))
+            viewController.call = call
+            DispatchQueue.main.async {
+                alertController.dismiss(animated: true, completion: nil)
+                UIViewController.topViewController?.present(viewController, animated: true, completion: nil)
+            }
+        })
+        let declineAction = UIAlertAction(title: "Decline", style: .destructive, handler: { (_) in
+            call.end()
+        })
+        
+        alertController.addAction(acceptAction)
+        alertController.addAction(declineAction)
+        
+        DispatchQueue.main.async {
+            UIViewController.topViewController?.present(alertController, animated: true, completion: nil)
+        }
+    }
+}
