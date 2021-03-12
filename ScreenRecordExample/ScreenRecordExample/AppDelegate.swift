abbd0e3f5272f5467478f4efd6bdc33547fde79e
Minhyuk Kim <kimminhyuk1004@gmail.com>
2021-03-12 19:28:26 +0900

    Fix error

--- ScreenRecordExample/ScreenRecordExample/AppDelegate.swift
+++ ScreenRecordExample/ScreenRecordExample/AppDelegate.swift
@@ -13,7 +13,7 @@ import Commons
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let appId: String = "add6749c-9165-48e4-abeb-bf58f7c399df"//<#Application ID From Sendbird Dashboard#>
+        let appId: String = <#Application ID From Sendbird Dashboard#>
         SendBirdCall.configure(appId: appId)
         SendBirdCall.addDelegate(self, identifier: "AppDelegate")
         
